85cbb50c7ab032a1e7edd4dba73d0b70f45dbdb9
이상민 <smlee@clamos.io>
2022-12-28 17:09:04 +0900

    Update parse.js

--- parse.js
+++ parse.js
@@ -1,18 +1,7 @@
 const fileName = `sample.bms`;
-const answer = [];
 
-fetch(fileName)
-.then(response => response.text())
-.then(rawData => {
-    for (let i = 0; i < 10; i++) {
-        answer.push(i);
-    }
-    // HEADER FIELD
-
-
-    // MAIN DATA FIELD
-
-
-});
-
-console.log(answer);
+async () => {
+    const res = await fetch(fileName);
+    const data = await res.text();
+    console.log(data);
+}

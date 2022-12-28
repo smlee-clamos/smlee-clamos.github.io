2668b4b79573a2300b598d106e837a1ece40d77e
이상민 <smlee@clamos.io>
2022-12-28 10:41:05 +0900

    js modified

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-Promise.all([
-    fetch('sample.txt').then(x => x.text()),
-  ]).then(([sampleResp]) => {
-    console.log(sampleResp);
-  });
+console.log(fetch('sample.txt').text());
+
+// Promise.all([
+//     fetch('sample.txt').then(x => x.text()),
+//   ]).then(([sampleResp]) => {
+//     console.log(sampleResp);
+//   });

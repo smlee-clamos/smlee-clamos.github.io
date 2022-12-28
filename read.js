772423c7803aea4a5f57db1a60eeb10f1574246b
이상민 <smlee@clamos.io>
2022-12-28 16:38:07 +0900

    update

--- read.js
+++ read.js
@@ -1,8 +1,10 @@
-const fileName = 'sample.bms';
+const fileName = `sample.bms`;
 let text = null;
 
 (async fileName => {
     const response = await fetch(fileName);
     const result = await response.text();
     text = result;
-})(fileName);
+})(fileName)
+.then(() => console.log(text))
+.catch(() => console.log(`failed`));

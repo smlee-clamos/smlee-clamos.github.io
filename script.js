1c6941bdee957371a6467b8e745a7867884ab582
이상민 <smlee@clamos.io>
2022-12-28 14:52:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const fileName = 'sample.bms';
-const text = readFile(fileName);
+const text = async fileName => {
+    const response = await fetch(fileName);
+    const text = await response.text();
+    console.log(text);
+    return text;
+}
+
+readFile(fileName);
 const data = [];
 
 // HEADER FIELD
 
 
 // MAIN DATA FIELD
-
-
-async function readFile(fileName) {
-    const response = await fetch(fileName);
-    const text = await response.text();
-    console.log(text);
-    return text;
-}

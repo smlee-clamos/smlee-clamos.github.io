25d0c27f32fc4eb4409bd93c89b3c77efa690115
이상민 <smlee@clamos.io>
2022-12-29 13:59:45 +0900

    update

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 makeChart(`sample.bms`);
 
 async function makeChart(fileName) {
-    console.log(1);
-
     const res = await fetch(fileName);
     const rawData = await res.text();
     const data = rawData.split(`\n`).map(element => element.trim()).filter(element => element !== ``);
     const chart = [];
 
-    console.log(data);
+    testFunc(data);
 
-    console.log(2);
     // *---------------------- HEADER FIELD
 
 
---
+++ show.js
@@ -0,0 +1,3 @@
+function testFunc(arr) {
+    console.log(arr);
+}

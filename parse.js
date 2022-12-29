6eb084cb230d86226717201b789fc2475646750b
이상민 <smlee@clamos.io>
2022-12-29 16:25:44 +0900

    Update parse.js

--- parse.js
+++ parse.js
@@ -5,11 +5,26 @@ async function makeChart(fileName) {
     const rawData = await res.text();
     const data = rawData.split(`\n`).map(element => element.trim()).filter(element => element !== ``);
     const chart = [];
-
-    testFunc(data);
-
-    // *---------------------- HEADER FIELD
-
-
-    // *---------------------- MAIN DATA FIELD
+    const info = new Object();
+    
+    let isHeader = true;
+    for (const datum of data) {
+        if (datum === `*---------------------- MAIN DATA FIELD`) {
+            isHeader = false;
+            chart.push(info);
+        }
+        else {
+            if (isHeader) {
+                const key = datum.slice(0, datum.indexOf(` `));
+                const value = datum.slice(datum.indexOf(` `) + 1);
+                
+                const infoCheck = [`#TITLE`, `#ARTIST`, `#BPM`, `#PLAYLEVEL`];
+                if (infoCheck.includes(key))
+                    info[key] = value;
+            } else {
+                const [key, value] = datum.split(`:`);
+                console.log(`${key}: ${value}`);
+            }
+        }
+    }
 }

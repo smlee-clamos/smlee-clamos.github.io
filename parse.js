6532634ee36d69767cde755c00a759ff7ba58b2c
이상민 <smlee@clamos.io>
2023-01-02 14:35:34 +0900

    Update parse.js

--- parse.js
+++ parse.js
@@ -6,26 +6,36 @@ async function makeChart(fileName) {
     const res = await fetch(fileName);
     const rawData = await res.text();
     const data = rawData.split(`\n`).map(element => element.trim()).filter(element => element !== ``);
-    const info = new Object();
-    
+    const obj = {};
+
     let isHeader = true;
     for (const datum of data) {
-        if (datum === `*---------------------- MAIN DATA FIELD`) {
-            isHeader = false;
-            chart.push(info);
-        }
-        else {
-            if (isHeader) {
-                const key = datum.slice(1, datum.indexOf(` `));
-                const value = datum.slice(datum.indexOf(` `) + 1);
-                
-                const infoCheck = [`TITLE`, `ARTIST`, `BPM`, `PLAYLEVEL`];
-                if (infoCheck.includes(key))
-                    info[key] = value;
-            } else {
-                const [key, value] = datum.split(`:`);
-                console.log(`${key}: ${value}`);
+        if (datum === `*---------------------- HEADER FIELD`) {
+            // Start Header Field
+            chart.push(obj);
+        } else if (isHeader) {
+            // Parse Header Field
+            const key = datum.slice(1, datum.indexOf(` `));
+            const value = datum.slice(datum.indexOf(` `) + 1);
+            
+            if ([`TITLE`, `ARTIST`, `BPM`, `PLAYLEVEL`].includes(key)) {
+                chart[0][key] = value;
             }
+        } else if (datum === `*---------------------- MAIN DATA FIELD`) {
+            // Start Main Data Field
+            isHeader = false;
+        } else {
+            // Parse Main Data Field
+            const [key, value] = datum.split(`:`);
+            const measure = key.slice(1, 4);
+            const lane = key.slice(4);
+
+            if (!chart[measure]) {
+                const obj = {};
+                chart.push(obj);
+            }
+            
+            chart[measure][lane] = value;
         }
     }
 }

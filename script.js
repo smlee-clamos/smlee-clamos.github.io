596c5320ccd4c60f523fe78f0ab40bee1baa997d
이상민 <smlee@clamos.io>
2023-02-09 17:03:22 +0900

    update

--- script.js
+++ script.js
@@ -9,5 +9,17 @@ export default {
         toggle() {
             this.awesome = !this.awesome
         }
-    }
+    },
+    template: `
+        <button class="btn btn-primary" @click="toggle">{{ text }}</button>
+
+        <div v-if="awesome">
+            <h1>멋진</h1>
+            <p>단락</p>
+        </div>
+        <div v-else>
+            <h1>멋지지 않은</h1>
+            <h2>제목</h2>
+        </div>
+        `
 }

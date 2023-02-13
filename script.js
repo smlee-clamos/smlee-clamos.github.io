b477f20131ba60f856d27915b8e0735993a1f34e
이상민 <smlee@clamos.io>
2023-02-13 14:23:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,20 @@ export default {
             this.awesome = !this.awesome
         }
     },
-    template: `
-        <button class="btn btn-primary" @click="toggle">{{ text }}</button>
-
-        <div v-if="awesome">
-            <h1>멋진</h1>
-            <p>단락</p>
-        </div>
-        <div v-else>
-            <h1>멋지지 않은</h1>
-            <h2>제목</h2>
-        </div>
-        `
+    template: {
+        templateText
+    }
 }
+
+const templateText = `
+<button class="btn btn-primary" @click="toggle">{{ text }}</button>
+
+<div v-if="awesome">
+    <h1>멋진</h1>
+    <p>단락</p>
+</div>
+<div v-else>
+    <h1>멋지지 않은</h1>
+    <h2>제목</h2>
+</div>
+`
